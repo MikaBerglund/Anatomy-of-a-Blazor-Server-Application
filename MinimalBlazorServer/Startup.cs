e6abdea99b067f61d75cdeae97892e27c410bbba
Mika Berglund <mika.berglund@meteoriitti.com>
2019-10-03 11:26:57 +0300

    Wired up Blazor in the empty ASP.NET Core application. The application is minimal, but still fully functional.

--- MinimalBlazorServer/Startup.cs
+++ MinimalBlazorServer/Startup.cs
@@ -16,6 +16,8 @@ namespace MinimalBlazorServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +28,15 @@ namespace MinimalBlazorServer
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
             });
         }
     }
